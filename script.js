49d65677e28c74448c2a83996a4a9b824adf81e8
Gustavo Santana <95528216+gustavsant@users.noreply.github.com>
2022-07-24 16:37:35 -0300

    agr ta legalzinho..

--- script.js
+++ script.js
@@ -1,29 +1,41 @@
-function clickado(id){
-    const opts = ['pedra', 'papel', 'tesoura']
-    let pcOpt = opts[Math.floor(Math.random() * opts.length)]
-    const resDiv = document.getElementById('res')
-    const winnerDiv = document.getElementById('winner')
-    
+const opts = document.querySelectorAll('.escolha')
 
-    if(id == 'pedra' && pcOpt == 'papel'){
-        winnerDiv.innerHTML = ''
-        winnerDiv.innerHTML += '<br>Vencedor: PC'
-    } else if(id == 'papel' && pcOpt == 'tesoura'){
-        winnerDiv.innerHTML = ''
-        winnerDiv.innerHTML += '<br>Vencedor: PC'
-    } else if(id == 'tesoura' && pcOpt == 'pedra'){
-        winnerDiv.innerHTML = ''
-        winnerDiv.innerHTML += '<br>Vencedor: PC'
-    } else if(id == pcOpt){
-        winnerDiv.innerHTML = ''
-        winnerDiv.innerHTML += '<br>EMPATE!'
-    }else{
-        winnerDiv.innerHTML = ''
-        winnerDiv.innerHTML += '<br>Vencedor: Você'
+const resDiv = document.getElementById('res')
+const winnerDiv = document.getElementById('winner')
+
+
+let playerEscolha
+let pcEscolha
+opts.forEach(escolha => escolha.addEventListener('click',(e) => {
+    playerEscolha = e.target.id
+    EscolhaDoPC()
+    resDiv.innerHTML = `${playerEscolha} <br>${pcEscolha}`
+    GerarResultados()
+    console.log('player' + playerEscolha)
+}))
+
+function GerarResultados(){
+    if(playerEscolha != undefined && pcEscolha != undefined){
+        if(playerEscolha == 'pedra' && pcEscolha == 'papel'){
+            writer('', '<br>Vencedor: PC')
+        } else if(playerEscolha == 'papel' && pcEscolha == 'tesoura'){
+            writer('', '<br>Vencedor: PC')
+        } else if(playerEscolha == 'tesoura' && pcEscolha == 'pedra'){
+            writer('', '<br>Vencedor: PC')
+        } else if(playerEscolha == pcEscolha){
+            writer('', '<br>EMPATE')
+        }else{
+            writer('', '<br>Vencedor: Você')
+        }
     }
-    
-    resDiv.innerHTML = 'RESULTADO <br>'
-    resDiv.innerHTML += `<br><p>Você: ${id}</p>`
-    resDiv.innerHTML += `<p>PC: ${pcOpt}<p/>`
+}
 
+function EscolhaDoPC(){
+    const ops = ['pedra', 'papel', 'tesoura']
+    pcEscolha = ops[Math.floor(Math.random() * ops.length)]
+}
+
+function writer(blank, result){
+    winnerDiv.innerHTML = ''
+    winnerDiv.innerHTML += result
 }
